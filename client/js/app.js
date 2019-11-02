5421742d28e5260398f58c38f767682c1271d8ae
narcomarch <kramoogle@gmail.com>
2019-11-02 17:35:16 +0100

    feat(client): add timer animation

--- client/js/app.js
+++ client/js/app.js
@@ -14,8 +14,21 @@ $(document).ready(function () {
     createButton.click(() => createScore());
     $("body").append(createButton);
 
+    //let timerId = setInterval(() => increaseProgressBar(), 1000);
     game.init(document.querySelectorAll('.card'));
 
+    var progressBar = $('#progress-bar'), width = 0;
+    progressBar.width(width);
+
+    var interval = setInterval(function () {
+        width += 1;
+        $('#progress-bar').width(width);
+        if (width >= 100) {
+            clearInterval(interval);
+        }
+    }, 100);
+
+    console.log($("#progress-bar"));
 });
 
 function readScore() {
@@ -26,4 +39,10 @@ function createScore() {
     // var currentDate = new Date();
     // scoreApi.create(currentDate, currentDate, Math.floor(Math.random() * 100000), "1")
     scoreApi.create("2019-10-17", "2019-10-14", Math.floor(Math.random() * 100000), "1")
+}
+
+function increaseProgressBar() {
+    let current = document.getElementsByClassName("progress-bar")[0].style.width;
+    console.log(current);
+    document.getElementsByClassName("progress-bar")[0].style.width = "70%";
 }
